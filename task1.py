d638242c28c31f6ab7b033b9bad7e62372fcaeb3
K-Haritha-Naidu <k.haritha.naidu.632009@gmail.com>
2026-06-08 09:02:28 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,7 @@
+# Print your name, age and city using 3 separate print statements
+name = input("enter your name")
+age = input("enter your age")
+city = input("enter your city")
+print(name)
+print(age)
+print(city)
