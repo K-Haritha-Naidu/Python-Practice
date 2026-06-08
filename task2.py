982ca1820988c0b2b7ca6c2e18bf714359603a45
K-Haritha-Naidu <k.haritha.naidu.632009@gmail.com>
2026-06-08 09:05:57 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,5 @@
+# Ask user for two numbers and print their sum
+num1 = int(input("enter your number 1"))
+num2 = int(input("enter your number 2"))
+sum = num1 + num2
+print("the sum of given two numbers is", sum)
