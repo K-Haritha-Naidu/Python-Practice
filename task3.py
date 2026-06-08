3ed6a7548664a630209196ee39c386762560840c
K-Haritha-Naidu <k.haritha.naidu.632009@gmail.com>
2026-06-08 09:09:38 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,6 @@
+#Ask user for a number and check if it's even or odd
+num = int(input("enter your number :"))
+if num % 2 == 0:
+    print("the given number is even")
+else:
+    print("the given number is odd")
