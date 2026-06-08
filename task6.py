7cfb31a8271d355b45f90c0e5fc91f181e242272
K-Haritha-Naidu <k.haritha.naidu.632009@gmail.com>
2026-06-08 09:33:11 +0530

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,8 @@
+# Create a list of 5 cities and print each one
+list1 = ["jaipur", "chennai", "kota", "coimbatore", "kanchipuram"]
+print(list1)
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
