69ab8c774d423c8898b100f5a9cb43b866653131
K Haritha <k.haritha.naidu.632009@gmail.com>
2026-06-11 22:24:51 +0530

    Create Functions(Celsius to Fahrenheit ).py

---
+++ Functions(Celsius to Fahrenheit ).py
@@ -0,0 +1,7 @@
+#Function that converts Celsius to Fahrenheit
+celsius = float(input("enter the temperature in celsius"))
+def celsius_to_farenheit(celsius):
+    farenheit = (celsius*(9/5))+32
+    print("the temperature in farenheit is:",farenheit)
+celsius_to_farenheit(celsius)
+     
